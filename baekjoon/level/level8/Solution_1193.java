4e27d7707a760c0040db445b005bfec73bdfba9e
myKim <myung120@gmail.com>
2018-09-12 00:53:13 +0900

    level8 commit

--- baekjoon/level/level8/Solution_1193.java
+++ baekjoon/level/level8/Solution_1193.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 public class Solution_1193 {
 
@@ -13,27 +12,41 @@ public class Solution_1193 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		char[] N = br.readLine().toCharArray();
+		int X = Integer.parseInt(br.readLine());
 		
-		int[] array = new int[10];
-		Arrays.fill(array, 0);
-		
-		for(int i=0; i<N.length; i++) {
-			int num = N[i]-'0';
-			if(num == 9) num = 6;  
-			array[num]++;
-		}
-		array[6] = (array[6] + 1) / 2;
-		
-		int max = 0;
-		for(int i=0; i<array.length; i++) {
-			if(max < array[i]) {
-				max = array[i];
-			}
-		}
-		bw.write(String.valueOf(max));
+		bw.write(getFraction(X));
 		
 		br.close();
 		bw.close();
+	}
+	
+	public static String getFraction(int X) {
+		int level = 1;  // 분수 단계
+		int cnt = 1;    // 해당 단계까지 원소 개수
+		
+		while(cnt < X) {
+			level++;
+			cnt += level;
+		}
+		
+		// 짝수 : 아래방향(↙)    / 홀수 : 윗방향(↗)
+		int num1, num2; 
+		num1 = 1;
+		num2 = level;
+		
+		while(X != cnt) {
+			num1++;
+			num2--;
+			cnt--;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		if(level % 2 == 0) {  // 짝수
+			sb.append(num2).append("/").append(num1);
+		}
+		else {  // 홀수
+			sb.append(num1).append("/").append(num2);
+		}
+		return sb.toString();
 	}
 }
