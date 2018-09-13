d84536bec13bb1fd7e55c63c1b5440224e105945
myKim <myung120@gmail.com>
2018-09-13 15:22:44 +0900

    BaekJoon algorithm solution commit

---
+++ baekjoon/etc/Solution_10101.java
@@ -0,0 +1,42 @@
+package my.etc;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Solution_10101 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int[] d = new int[3];
+		d[0] = Integer.parseInt(br.readLine());
+		d[1] = Integer.parseInt(br.readLine());
+		d[2] = Integer.parseInt(br.readLine());
+		
+		bw.write(checkDegree(d));
+		
+		br.close();
+		bw.close();
+	}
+	
+	public static String checkDegree(int[] d) {
+		if(d[0] + d[1] + d[2] == 180) {
+			if(d[0] == d[1] && d[1] == d[2]) {
+				return "Equilateral";
+			}
+			else if(d[0] == d[1] || d[0] == d[2] || d[1] == d[2]) {
+				return "Isosceles";
+			}
+			else {
+				return "Scalene";
+			}
+		}
+		else {
+			return "Error";
+		}
+	}
+}
---
+++ baekjoon/etc/Solution_11365.java
@@ -0,0 +1,29 @@
+package my.etc;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Solution_11365 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		while(true) {
+			String line = br.readLine();
+			
+			if(line.equals("END")) {
+				break;
+			}
+			
+			bw.write(new StringBuilder(line).reverse().toString());
+			bw.newLine();
+		}
+		
+		br.close();
+		bw.close();
+	}
+}
