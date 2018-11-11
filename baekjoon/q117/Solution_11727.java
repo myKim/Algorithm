8bf45bbcb17a894095079075a86cb2524d18eb72
myKim <myung120@gmail.com>
2018-11-11 21:27:22 +0900

    upload source

---
+++ baekjoon/q117/Solution_11727.java
@@ -0,0 +1,31 @@
+package my.baekjoon.q117;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution_11727 {
+	
+	private static final int DIV = 10_007;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[n];
+		
+		dp[0] = 1;
+		if(n > 1) {
+			dp[1] = 3;
+			
+			for(int i=2; i<dp.length; i++) {
+				dp[i] = (dp[i-1] + dp[i-2]*2) % DIV;
+			}
+		}
+		
+		System.out.println(dp[n-1]);
+		
+		br.close();
+	}
+	
+}
