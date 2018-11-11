99f85c84d7f0326893814f3a981312e373b78a97
myKim <myung120@gmail.com>
2018-11-11 16:27:47 +0900

    upload source

---
+++ baekjoon/q24/Solution_2480.java
@@ -0,0 +1,37 @@
+package my.baekjoon.q24;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution_2480 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] arr = new int[3];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		int res = 0;
+		if(arr[0]==arr[1] && arr[1]==arr[2]) {
+			res = 10000 + arr[0]*1000;
+		}
+		else if(arr[0]==arr[1] || arr[1]==arr[2]) {
+			res = 1000 + arr[1]*100;  // 가운데 숫자는 항상 같은 숫자
+		}
+		else {
+			res = arr[2]*100;
+		}
+		
+		System.out.print(res);
+		
+		br.close();
+	}
+}
