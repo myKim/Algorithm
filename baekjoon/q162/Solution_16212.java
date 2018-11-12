8b08aa2264750b11287fc3c9079cb9739535be8a
myKim <myung120@gmail.com>
2018-11-12 12:30:08 +0900

    upload source

---
+++ baekjoon/q162/Solution_16212.java
@@ -0,0 +1,34 @@
+package my.baekjoon.q162;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution_16212 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		for(int i : arr) {
+			bw.write(i + " ");
+		}
+		
+		br.close();
+		bw.close();
+	}
+	
+}
